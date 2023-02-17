d4fb2e1a082d8b2a3457c99821e934ffa2caff87
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2023-02-17 13:01:50 +0100

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,47 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { DefaultSeo } from 'next-seo';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+  // track page views
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-01Q6PSBFGE"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_GOOGLE_ANALYTICS_TRACKING_CODE}', {
+            page_path: window.location.pathname,
+        });
+        `,
+        }}
+      />
       <DefaultSeo
         title="Ping Telecoms"
         description="Ping Telecommunications Resources Limited is an indigenous ICT company incorporated in Nigeria to provide cost effective and customer dependent services without compromising standards."
